e4621345c1bf937b5200362d0d10dc171bc8ea5f
Adam Ševčík <56029806+Aletchery@users.noreply.github.com>
2020-12-11 10:50:32 +0100

    Add files via upload

--- boxElement.js
+++ boxElement.js
@@ -1,13 +1,26 @@
+const boxes = require('./index');
 
+    
 class BoxElement extends HTMLDivElement {
     constructor() {
         super();
-    
-        var d = document.createElement(div);
+        const d = document.createElement('template');
+        
 
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.appendChild(d.content.cloneNode(true));
        
       }
-   
+    }
+    customElements.define("box-element", BoxElement,{extends : 'div'});
+
   
-  }
-  customElements.define("box-element", BoxElement, {extends: 'div'});
+
+  /*for(var i = 0;i<boxes.length;i++){
+      boxCreator(boxes[i]);
+        var box = document.createElement("box-element");
+        var body = document.getElementById("body");
+        body.appendChild(box);
+  }*/
+
+  
