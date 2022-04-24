1a322a4909b2392d76a369819f9f6241a483f2a1
Adam Ševčík <56029806+Aletchery@users.noreply.github.com>
2022-04-24 18:38:22 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
+/*  config.js
+    Adam Sevcik 2022
+    FitLayout web   */ 
+
+// Set repository ID for the app to be started with
 const config = {
-    repo_id: "7d1a35b2-22dd-451f-9c6b-476c821b8c9b"
+    repo_id: "7d1a35b2-22dd-451f-9c6b-476c821b8c9b" //Write repo id here
    };
    module.exports = config;
