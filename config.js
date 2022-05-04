c6b8fb93a2a8a9612156b2d15f2b3a5b7dfc0483
Adam Ševčík <56029806+Aletchery@users.noreply.github.com>
2022-05-04 19:39:40 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -4,6 +4,7 @@
 
 // Set repository ID for the app to be started with
 const config = {
-    repo_id: "7d1a35b2-22dd-451f-9c6b-476c821b8c9b" //Write repo id here
+    repo_id: "12425e9f-6cdd-4700-8e35-6a4c6504a258", // Write repo id here (another example 7d1a35b2-22dd-451f-9c6b-476c821b8c9b)
+    url: "https://layout.fit.vutbr.cz/api" // Write base url here
    };
    module.exports = config;
